f6c819c267670a0b7ee573cd7209ecfff1f73be7
Djalma Araújo <djalma.araujo@gmail.com>
2018-10-23 15:58:04 +0100

    fix path

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 
 import TodoApp from "./TodoApp";
-import "/style/index.css";
+import "./style/index.css";
 render(<TodoApp />, document.querySelector("#root"));
